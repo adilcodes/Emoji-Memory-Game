17623a709915f4fe1f865f2ed1ccfd37ec960754
Adil Sheikh <ascodesofficial@gmail.com>
2024-02-24 17:06:47 +0500

    Fetch the data from the json file and make the placeholder functions for the shuffling data and card creating

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
-// Targetting DOM
-let single_card_array = document.querySelectorAll(".single-card");
+// Variables
+let score = 0;
+let cards_data = [];
 
+// Fetch Data Using async/await
+const FetchingData = async () => {
+    try {
+        let response = await fetch("./data/data.json");
+        if(response.ok){
+            let result = await response.json();
+            cards_data = [...result, ...result];
+            ShuffleData(cards_data);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Run Function on load
+FetchingData();
+
+// Shuffling Data Function
+const ShuffleData = (data) => {
+    // Shuffling The Data
+    console.log(data);
+
+    // Creating Cards
+    CreateCards();
+}
+
+// Creating Cards Function
+const CreateCards = () => {
+    console.log("creating cards...");
+}
+
+// DOM Targetting
+let cards_container = document.querySelector(".cards-container");
+let single_card_array = document.querySelectorAll(".single-card");
 
 // Event Listeners
 single_card_array.forEach((single_card) => {
