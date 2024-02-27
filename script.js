b180b736c8be94b813eb386d5839f4590f8568ad
Adil Sheikh <ascodesofficial@gmail.com>
2024-02-28 00:18:27 +0500

    Change the emojis and make the shuffle and creating cards functionality

--- script.js
+++ script.js
@@ -2,45 +2,62 @@
 let score = 0;
 let cards_data = [];
 
+// DOM Targetting
+let cards_container = document.querySelector(".cards-container");
+
 // Fetch Data Using async/await
 const FetchingData = async () => {
-    try {
-        let response = await fetch("./data/data.json");
-        if(response.ok){
-            let result = await response.json();
-            cards_data = [...result, ...result];
-            ShuffleData(cards_data);
-        }
-    } catch (error) {
-        console.error(error);
+  try {
+    let response = await fetch("./data/data.json");
+    if (response.ok) {
+      let result = await response.json();
+      cards_data = [...result, ...result];
+      ShuffleData(cards_data);
     }
-}
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-// Run Function on load
+// Run Function on load - Starting
 FetchingData();
 
 // Shuffling Data Function
-const ShuffleData = (data) => {
-    // Shuffling The Data
-    console.log(data);
+const ShuffleData = (dataArray) => {
+  // Shuffling The Data
+  dataArray.sort(() => Math.random() - 0.5);
 
-    // Creating Cards
-    CreateCards();
-}
+  // Creating Cards
+  CreateCards(dataArray);
+};
 
 // Creating Cards Function
-const CreateCards = () => {
-    console.log("creating cards...");
-}
+const CreateCards = (cardData) => {
+  cardData = cardData.map((singleVal) => {
+    let singleCard = `<div class="single-card">
+        <div class="single-card-inner">
+            <div class="single-card-front">
+                <h1>🤔</h1>
+            </div>
+            <div class="single-card-back">
+                <h1>${singleVal.content}</h1>
+            </div>
+        </div>
+    </div>`;
 
-// DOM Targetting
-let cards_container = document.querySelector(".cards-container");
-let single_card_array = document.querySelectorAll(".single-card");
+    // Append
+    cards_container.innerHTML += singleCard;
+  });
 
-// Event Listeners
-single_card_array.forEach((single_card) => {
+  MakeCardsClickable();
+};
+
+const MakeCardsClickable = () => {
+  let single_card_array = document.querySelectorAll(".single-card");
+  single_card_array.forEach((single_card) => {
     single_card.addEventListener("click", () => {
-        let single_card_inner = single_card.querySelector(".single-card-inner");
-        single_card_inner.classList.add("single-card-inner-rotate");
+      let single_card_inner = single_card.querySelector(".single-card-inner");
+      single_card_inner.classList.add("single-card-inner-rotate");
     });
-});
+  });
+};
