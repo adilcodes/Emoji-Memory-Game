e2c17a4b1a17d740ea7fe1e5a1219853b2469fe5
Adil Sheikh <ascodesofficial@gmail.com>
2024-03-04 00:07:48 +0500

    Made the functionality to check if the cards match each other or not

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Variables
 let score = 0;
 let cards_data = [];
+let counter = 0;
+let rotatedCards = [];
 
 // DOM Targetting
 let cards_container = document.querySelector(".cards-container");
@@ -36,10 +38,10 @@ const CreateCards = (cardData) => {
   cardData = cardData.map((singleVal) => {
     let singleCard = `<div class="single-card">
         <div class="single-card-inner">
-            <div class="single-card-front">
+            <div class="single-card-back">
                 <h1>🤔</h1>
             </div>
-            <div class="single-card-back">
+            <div class="single-card-front">
                 <h1>${singleVal.content}</h1>
             </div>
         </div>
@@ -55,9 +57,45 @@ const CreateCards = (cardData) => {
 const MakeCardsClickable = () => {
   let single_card_array = document.querySelectorAll(".single-card");
   single_card_array.forEach((single_card) => {
-    single_card.addEventListener("click", () => {
+    single_card.addEventListener("click", function () {
       let single_card_inner = single_card.querySelector(".single-card-inner");
-      single_card_inner.classList.add("single-card-inner-rotate");
+      if (counter < 2) {
+        if (!(single_card_inner.classList.contains("single-card-inner-rotate"))) {
+          single_card_inner.classList.add("single-card-inner-rotate");
+          CardMatchChecker(this.querySelector(".single-card-front h1").innerText);
+        }
+      }
     });
   });
 };
+
+function CardMatchChecker(cardEmoji) {
+  if (counter < 1) {
+    counter++;
+    rotatedCards.push(cardEmoji);
+  } else if (counter == 1 && counter < 2) {
+    counter++;
+    rotatedCards.push(cardEmoji);
+    let rotated_cards = document.querySelectorAll(".single-card-inner-rotate:not(.already-rotated)");
+    if (rotatedCards[0] === rotatedCards[1]) {
+      setTimeout(() => {
+        rotated_cards.forEach((single_card) => {
+          single_card.classList.add("already-rotated");
+          single_card.querySelector(".single-card-front").classList.add("matched-cards");
+        });
+        score++;
+        document.getElementById("score").innerText = score;
+        counter = 0;
+        rotatedCards = [];
+      }, 1500);
+    } else {
+      setTimeout(() => {
+        rotated_cards.forEach((single_card) => {
+          single_card.classList.remove("single-card-inner-rotate");
+        });
+        counter = 0;
+        rotatedCards = [];
+      }, 1500);
+    }
+  }
+}
